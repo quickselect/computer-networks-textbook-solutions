f04fc3e1fefe1f33f50ba923896585c27d49024a
quickselect <quickselect@proton.me>
2024-02-20 01:27:55 +0300

    indentation fixed... again!

--- chapter-1/server.c
+++ chapter-1/server.c
@@ -9,29 +9,29 @@
 #define MAX_LINE 256
 
 int main() {
-	struct sockaddr_in sin;
-	char buf[MAX_LINE];
-	int buf_len, addr_len;
-	int s, new_s;
+  struct sockaddr_in sin;
+  char buf[MAX_LINE];
+  int buf_len, addr_len;
+  int s, new_s;
 
-	/* build address data structure */
-	bzero((char *)&sin, sizeof(sin));
-	sin.sin_family = AF_INET;
-	sin.sin_addr.s_addr = INADDR_ANY;
-	sin.sin_port = htons(SERVER_PORT);
+  /* build address data structure */
+  bzero((char *)&sin, sizeof(sin));
+  sin.sin_family = AF_INET;
+  sin.sin_addr.s_addr = INADDR_ANY;
+  sin.sin_port = htons(SERVER_PORT);
 
-	/* setup passive open */
-	if ((s = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
-	perror("simplex-talk: socket");
-	exit(1);
-	}
-	if ((bind(s, (struct sockaddr *)&sin, sizeof(sin))) < 0) {
-	perror("simplex-talk: bind");
-	exit(1);
-	}
-	listen(s, MAX_PENDING);
+  /* setup passive open */
+  if ((s = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+    perror("simplex-talk: socket");
+    exit(1);
+  }
+  if ((bind(s, (struct sockaddr *)&sin, sizeof(sin))) < 0) {
+    perror("simplex-talk: bind");
+    exit(1);
+  }
+  listen(s, MAX_PENDING);
 
-	/* wait for connection, then receive and print text */
+  /* wait for connection, then receive and print text */
   while(1) {
     if ((new_s = accept(s, (struct sockaddr *)&sin, &addr_len)) < 0)
     {
